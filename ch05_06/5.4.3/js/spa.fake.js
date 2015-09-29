8484fa190daffa042e505f29516b64a9a6165ca3
capouch <brianc@palaver.net>
2015-09-29 01:33:43 -0400

    This change is coming from VS Code

--- ch05_06/5.4.3/js/spa.fake.js
+++ ch05_06/5.4.3/js/spa.fake.js
@@ -23,22 +23,22 @@ spa.fake = (function () {
 
   getPeopleList = function () {
     return [
-      { name : 'Betty', _id : 'id_01',
+      { name : 'Larry', _id : 'id_01',
         css_map : { top: 20, left: 20,
           'background-color' : 'rgb( 128, 128, 128)'
         }
       },
-      { name : 'Mike', _id : 'id_02',
+      { name : 'Pablo', _id : 'id_02',
         css_map : { top: 60, left: 20,
           'background-color' : 'rgb( 128, 255, 128)'
         }
       },
-      { name : 'Pebbles', _id : 'id_03',
+      { name : 'Ronnie', _id : 'id_03',
         css_map : { top: 100, left: 20,
           'background-color' : 'rgb( 128, 192, 192)'
         }
       },
-      { name : 'Wilma', _id : 'id_04',
+      { name : 'Sal', _id : 'id_04',
         css_map : { top: 140, left: 20,
           'background-color' : 'rgb( 192, 128, 128)'
         }
