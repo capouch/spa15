783559cdf07deb18e8063f07d5928c620fc4b75b
capouch <brianc@palaver.net>
2015-10-23 15:42:01 -0400

    Make sure I don't screw up again

--- ch08/8.3.2-mongo-connect/app.js
+++ ch08/8.3.2-mongo-connect/app.js
@@ -1,5 +1,5 @@
 /*
- * app.js - Express server with routing
+ * app.js - Express server with advanced routing
 */
 
 /*jslint         node    : true, continue : true,
@@ -13,35 +13,70 @@
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
 var
-  http    = require( 'http'     ),
-  express = require( 'express'  ),
-  routes  = require( './routes' ),
+  http    = require( 'http'    ),
+  express = require( 'express' ),
+  morgan = require( 'morgan' ),
+  bodyParser = require( 'body-parser' ),
+  methodOverride = require( 'method-override' ),
+  errorHandler = require( 'errorhandler' ),
 
   app     = express(),
   server  = http.createServer( app );
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 // ------------- BEGIN SERVER CONFIGURATION ---------------
-app.configure( function () {
-  app.use( express.bodyParser() );
-  app.use( express.methodOverride() );
+  app.use( bodyParser.urlencoded() );
+  app.use( methodOverride() );
   app.use( express.static( __dirname + '/public' ) );
-  app.use( app.router );
+
+  app.use( morgan( 'common' ) );
+  app.use( errorHandler() );
+//  app.use( errorHandler({
+//  dumpExceptions : true,
+//    showStack      : true
+// });
+
+// all configurations below are for routes
+app.get( '/', function ( request, response ) {
+  response.redirect( '/spa.html' );
 });
 
-app.configure( 'development', function () {
-  app.use( express.logger() );
-  app.use( express.errorHandler({
-    dumpExceptions : true,
-    showStack      : true
-  }) );
+app.all( '/user/*?', function ( request, response, next ) {
+  response.contentType( 'json' );
+  next();
 });
 
-app.configure( 'production', function () {
-  app.use( express.errorHandler() );
+app.get( '/user/list', function ( request, response ) {
+  response.send({ title: 'user list' });
 });
 
-routes.configRoutes( app, server );
+app.post( '/user/create', function ( request, response ) {
+  response.send({ title: 'user created' });
+});
+
+app.get( '/user/read/:id([0-9]+)',
+  function ( request, response ) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' found'
+    });
+  }
+);
+
+app.post( '/user/update/:id([0-9]+)',
+  function ( request, response ) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' updated'
+    });
+  }
+);
+
+app.get( '/user/delete/:id([0-9]+)',
+  function ( request, response ) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' deleted'
+    });
+  }
+);
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
